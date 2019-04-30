34c6fb870529e25707ce35d475c03a28ebf81887
cliffckerr <questionably@gmail.com>
2019-04-30 04:21:41 +0100

    omg that worked!

--- brainstaves/makelivescore.py
+++ brainstaves/makelivescore.py
@@ -57,7 +57,7 @@ def makequartet():
     va = instruments.Section(name='va', instrument='viola')
     vc = instruments.Section(name='vc', instrument='cello')
     quartet = [v1,v2,va,vc]
-    qd = sc.objdict({inst.name:inst for inst in quartet})
+    qd = sc.objdict([(inst.name,inst) for inst in quartet])
     return quartet,qd
 
 
@@ -67,47 +67,46 @@ if 'load' in torun:
     print('Loading XML')
     xml = musescore.XML()
     nd = sc.objdict() # For storing all the notes
+    nd.notes = []
 
 
 if 'sectionB' in torun:
     print('Creating section B')
     quartet,qd = makequartet()
-    
     nd['B'] = sc.objdict()
-    
     nd.B.startstop = sc.objdict()
-    nd.B.startstop['v1'] = [39,40]
-    nd.B.notes = xml.loadnotes(part='v1', measurerange=nd.B.startstop['v1'])
     
-    length = 20 # How many bars it's supposed to be
-    for inst in quartet:
+    for part,inst in qd.items():
         inst.mindur = 8
         inst.timesig = '12/8'
         inst.nbars = 1
         inst.refresh()
+        
+        if part == 'v1':
+            nd.B.startstop[part] = [39,40]
+            probs = [1/12]*4 + [2/12]*2 + [3/12]*2 + [i/12 for i in range(4,12)] + [1]*4
+        else:
+            nd.B.startstop[part] = [21,40]
+            probs = [1,1]
     
-    probs = [1/12]*4 + [2/12]*2 + [3/12]*2 + [i/12 for i in range(4,12)] + [1]*4
-    count = 0
-    assert len(probs) == length
-    for inst in quartet:
+        nd.B[part] = xml.loadnotes(part=part, measurerange=nd.B.startstop[part])
+        
         for prob in probs:
             inst.seed += 1
             startval = inst.score[-1] if inst.scorepts else 'min'
             inst.brownian(maxstep=2, startval=startval, skipstart=True)
             inst.seed += 1
             inst.cat()
-
-
-if 'create' in torun:
-    print('Creating notes')
-    for n,orignote in enumerate(nd.B.notes): # WARNING FIX
-        print('%s. %s' % (n, instruments.num2char(qd.v1.score[n])))
-        nd.B.notes[n] = xmlnote(orignote, qd.v1.score[n])
+    
+        for n,orignote in enumerate(nd.B[part]):
+            print('%s. %s' % (n, instruments.num2char(qd.v1.score[n])))
+            note = xmlnote(orignote, qd.v1.score[n])
+            nd.notes.append(note)
 
 
 if 'write' in torun:
     print('Writing XML')
-    xml.write(data=nd.B.notes, verbose=True)
+    xml.write(data=nd.notes, verbose=True)
 
 
 sc.toc()
