5f9e4ca0633aabfe0b1b759ec5f4163c6d657913
cliffckerr <questionably@gmail.com>
2019-05-01 12:47:25 +0100

    changes for silverdale

--- brainstaves/plotsound.py
+++ brainstaves/plotsound.py
@@ -1,33 +1,39 @@
+import os
 import sounddevice as sd
 import pylab as pl
 import spectrogram as s
 
 duration = 1
 fs = 44100
+saveupload = False
+
 
 arrs = []
 
-print('Recording...')
+print('Recording, Ctrl+C to stop...')
 stop = False
 while not stop:
     try:
         tmp = sd.rec(int(duration * fs), samplerate=fs, channels=2, blocking=True)
         arrs.append(tmp)
     except:
+        print('Stopping...')
         stop = True
 
 arr = pl.concatenate(arrs)
 
 print('Plotting...')
-pl.plot(arr)
+xax = pl.arange(len(arr))/float(fs)
+pl.plot(xax, arr)
+pl.xlabel('Time (s)')
+
 ims = s.plotstft(arr)
-#pl.ylim([0,1000])
 pl.pause(0.2)
 
-#import os
-#print('Saving...')
-#pl.savefig('tmp.png')
-#print('Publishing...')
-#os.system('scp -r "tmp.png" cliffker@cliffkerr.com:/home3/cliffker/public_html/thekerrlab/tmp/')
+if saveupload:
+    print('Saving...')
+    pl.savefig('tmp.png')
+    print('Publishing...')
+    os.system('scp -r "tmp.png" cliffker@cliffkerr.com:/home3/cliffker/public_html/thekerrlab/tmp/')
 
 print('Done.')
