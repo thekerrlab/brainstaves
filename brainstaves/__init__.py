9ad693e3d6f29bafa077f615b93b3ff8d322ee24
cliffckerr <questionably@gmail.com>
2019-05-08 19:54:59 +0100

    more renaming

--- brainstaves/__init__.py
+++ brainstaves/__init__.py
@@ -1,5 +1,5 @@
-from .musescore import XML
+from .bs_musescore import XML
 from .bs_music import *
+from .bs_makelivescore import makelivescore
 #from .bs_animation import *
-from .makelivescore import makelivescore
 from .bs_app import makeapp, run
