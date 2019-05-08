14b10d595d7666bc8ff73480e892ffe4993a85ca
cliffckerr <questionably@gmail.com>
2019-05-08 19:41:09 +0100

    ok, nevermind, separate app wont work

--- brainstaves/app/brainstaves.py
+++ brainstaves/app/brainstaves.py
@@ -9,47 +9,50 @@ Webserver that runs and refreshes Brainstaves. Some functions are deprecated.
 # Housekeeping
 ##################################
 
-__version__ = '2.0.0'
+def makeapp():
 
-# Imports
-print('Importing modules...')
-import sys
-import scirisweb as sw
-import sciris as sc
+    # Imports
+    print('Importing modules...')
+    import sys
+    import scirisweb as sw
+    import sciris as sc
 
-statusfile = 'status.tmp'
+    __version__ = '3.0.0'
+    statusfile = 'status.tmp'
 
-# Create the app
-print('Setting defaults...')
-if len(sys.argv)>1: port = sys.argv[1]
-else:               port = 8185
-app = sw.ScirisApp(__name__, name="Brainstaves", server_port=port) # Set to a nonstandard port to avoid collisions
-app.data = None # Initialize the results
+    # Create the app
+    print('Setting defaults...')
+    if len(sys.argv)>1: port = sys.argv[1]
+    else:               port = 8185
+    app = sw.ScirisApp(__name__, name="Brainstaves", server_port=port) # Set to a nonstandard port to avoid collisions
+    app.data = None # Initialize the results
+
+    ##################################
+    # Define the RPCs
+    ##################################
+
+    @app.register_RPC()
+    def get_status():
+        try:
+            with open(statusfile) as f:
+                output = f.read().rstrip()
+        except Exception as E:
+            print('Status file not found: %s' % str(E))
+            output = 'n/a'
+        return output
 
 
-##################################
-# Define the RPCs
-##################################
+    @app.register_RPC()
+    def get_version():
+        return __version__
 
-@app.register_RPC()
-def get_status():
-    try:
-        with open(statusfile) as f:
-            output = f.read().rstrip()
-    except Exception as E:
-        print('Status file not found: %s' % str(E))
-        output = 'n/a'
-    return output
-
-
-@app.register_RPC()
-def get_version():
-    return __version__
+    return app
 
 
 # Run the server
 if __name__ == "__main__":
     try: # WARNING -- this try-catch doesn't work
+        app = makeapp()
         app.run()
     except Exception as E:
         print('Shutting down server and removing status file and live files...')
