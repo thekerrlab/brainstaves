070b7860e4b0497e46f3b24fbbdf2adf310cd370
cliffckerr <questionably@gmail.com>
2019-05-09 00:12:02 +0100

    histogram looking good!

--- data/rawdatastats.py
+++ data/rawdatastats.py
@@ -11,3 +11,21 @@ import sciris as sc
 
 infile = 'data-may08.obj'
 
+df = sc.loadobj(infile)
+
+raw = df['rawValue'].astype(float)
+
+raw = raw[pl.nonzero(raw)]
+raw -= raw.mean()
+raw /= 0.7*raw.std()
+
+pl.figure()
+pl.plot(raw)
+
+pl.figure()
+x = pl.randn(len(raw))
+bins = pl.linspace(-8,8,201)
+pl.hist(raw, bins)
+pl.hist(x, bins, alpha=0.8)
+
+print('Done.')
